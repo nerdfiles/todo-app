8a8791f948c6914f4e029de3bba99459102d7370
nerdfiles <nerdfiles@gmail.com>
2021-07-17 11:45:25 -0500

    add api surface table

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,99 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { IonList, IonItem, IonButtons, IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 
-interface ContainerProps { }
+const sleep = (ms: number) => new Promise(res => setTimeout(res, ms))
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+type Props = {};
+
+interface IDefaultState {
+  items: any[]
+}
+
+const InitialState:IDefaultState = {
+  items: []
 };
+
+type State = typeof InitialState;
+
+class ExploreContainer extends Component<Props, State> {
+
+  public readonly state = InitialState;
+
+  queryReadListView() {
+
+    // @TODO use axios, setup redux, etc
+
+    const items = this.state.items;
+
+    const newItems = [
+      {
+        title: 'whatever note title',
+        content: 'whatever this says and then some'
+      },
+      {
+        title: 'whatever note title',
+        content: 'whatever this says and then some'
+      },
+      {
+        title: 'new item',
+        content: 'new item content'
+      }
+    ];
+
+    const newItemsList = [ ...newItems, ...items];
+
+    this.setState({ 
+      items: newItemsList 
+    });
+  }
+
+  async commandAdd(e: any) {
+
+    await sleep(1000)
+
+    console.log(e);
+  }
+
+  commandUpdate(e: any) {
+    console.log(e);
+  }
+
+  commandDelete(e: any) {
+    console.log(e);
+  }
+
+  componentDidMount() {
+    this.queryReadListView()
+  }
+
+  render() {
+    return (
+      <div className="container">
+
+        <IonButton onClick={(e) => this.commandAdd(e)}>Add</IonButton>
+
+        <IonList>
+          {
+            this.state.items && this.state.items.length ?
+              this.state.items.map((itemRef, keyRef) => {
+                return (
+                  <IonItem key={keyRef} className="IonItem--Top">
+                    <p title={itemRef.title}>{itemRef.content}</p>
+
+                    <IonButton onClick={(e) => this.commandUpdate(e)}>Update</IonButton>
+                    <IonButton onClick={(e) => this.commandDelete(e)}>Delete</IonButton>
+                  </IonItem>
+                );
+              })
+            : null
+          }
+        </IonList>
+
+      </div>
+    );
+
+  }
+}
 
 export default ExploreContainer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>t0d0</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
